409cff7b183befd8bb1ee2fe985a8d8eb8108dcb
NereaCassian <erodtanime@gmail.com>
2021-06-18 16:14:26 +0100

    Added a bot status

--- index.js
+++ index.js
@@ -164,4 +164,13 @@ client.on("message", async message => {
 
 });
 
+client.on('ready', () => {
+    console.log("Activity OK")
+    //CHANGE {type: 2} in 
+    //1 FOR PLAYING
+    //2 FOR LISTENING
+    //3 FOR WATCHING
+    client.user.setActivity("El fin del verano", {type: 2});
+});
+
 client.login();
