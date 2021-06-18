22bb12f4ce1b968abbf9769cfba84667037e3a76
NereaCassian <erodtanime@gmail.com>
2021-06-18 20:13:23 +0100

    added some commands

--- index.js
+++ index.js
@@ -206,7 +206,7 @@ client.on("message", async message => {
 
         const videoFinder = async (query) => {
 
-            const VideoResult = await ytSearch(query);
+            const videoResult = await ytSearch(query);
 
             return (videoResult.videos.length > 1) ? videoResult.videos(0) : null;
 
